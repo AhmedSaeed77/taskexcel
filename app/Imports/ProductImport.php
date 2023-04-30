04e7648eb2a0696d0025dbaa4e1c8a87337f34d2
unknown <AhmedSaeed77>
2023-04-30 12:13:41 +0300

    first

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -4,21 +4,36 @@ namespace App\Imports;
 
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-class ProductImport implements ToModel
+class ProductImport implements  ToModel, WithStartRow ,WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    public function startRow(): int
+    {
+        return 2;
+    }
+
+    public function headingRow(): int
+    {
+        return 1;
+    }
+
     public function model(array $row)
     {
+        $name = array_search('name',$row);
+        $type = array_search('type',$row);
+        $counter = array_search('counter',$row);
+
         return new Product([
-            "name" => $row[0],
-            "type" => $row[1],
-            "counter" => $row[2],
+            "name" => $row['name'],
+            "type" => $row['type'],
+            "counter" => intval($row['counter']) ,
         ]);
     }
 }
